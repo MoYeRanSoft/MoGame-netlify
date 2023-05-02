f1dd98dfd2ee358bb72f98e8e55e744d2230fd9d
MoYeRanSoft <120180159+MoYeRanSoft@users.noreply.github.com>
2023-05-02 14:19:36 +0800

    Add files via upload

---
+++ game/2048/joke/jss/6151/animation1.js
@@ -0,0 +1,46 @@
+//通过动画显示数字
+function showNumberWihthAnimation(i,j,randNumber){
+	//console.log(i,j,randNumber);
+	var numberCell=$('#number-cell-'+i+'-'+j);  //#符号很容易被忽视 ID
+	numberCell.css('background-color',getNumberBackgroundColor(randNumber));
+	numberCell.css('color',getNumberColor(randNumber));
+	var text = String(randNumber) //墨叶染千枝
+	if (text == '2') {
+		text = '刘';
+	}
+	if (text == '4') {
+		text = '一';
+	}
+	if (text == '8') {
+		text = '五';
+	}
+	if (text == '16') {
+		text = '一';
+	}
+	if (text == '32') {
+		text = '快';
+	}
+	if (text == '64') {
+		text = '乐';
+	}
+
+	numberCell.text(text);
+
+	numberCell.animate({
+		width:cellWidth,
+		height:cellWidth,
+		top:getPosTop(i,j),
+		left:getPosLeft(i,j)
+	},500);
+
+}
+
+//通过动画显示移动的效果
+function showMoveAnimation(fromx,fromy,tox,toy){
+	var numberCell=$('#number-cell-'+fromx+'-'+fromy);
+	numberCell.animate({
+		top:getPosTop(tox,toy),
+		left:getPosLeft(tox,toy)
+	},200);
+
+}
